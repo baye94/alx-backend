d909aab959d1b456837fa6bdfaa41c6a7436ef3a
baye94 <fallcheikh893@gmail.com>
2023-01-31 13:47:27 +0000

    i18n

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,38 +1,43 @@
 #!/usr/bin/env python3
 """
-Creating an i18n application
+a python module to initiate a flask app using Babel
 """
-
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-class Config():
-    """Configuration class for babel"""
-
+class Config(object):
+    """
+    a class to configure babel
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(app)
 
 
-@app.route("/")
-def home() -> str:
-    """Home page"""
+@babel.localeselector
+def get_locale():
+    """
+    get_locale - function to get the local selector
+    """
+    lcl = request.args.get('locale', None)
+    if lcl and lcl in app.config['LANGUAGES']:
+        return lcl
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def Welcome():
+    """
+    Welcome - a route to a 4-index html
+    """
     return render_template('4-index.html')
 
 
-@babel.localeselector
-def get_locale() -> str:
-    """Select Language"""
-    locale = request.args.get('locale')
-    if locale and locale in Config.LANGUAGES:
-        return locale
-    return request.accept_languages.best_match(Config.LANGUAGES)
-
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host="0.0.0.0", port="5000")
