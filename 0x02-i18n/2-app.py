8d85f256ff0d7221bec9f0c0c7c9c677a3663216
baye94 <fallcheikh893@gmail.com>
2023-01-31 13:43:16 +0000

    i18n

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
-"""
-Creating an i18n application that supports different languages
-"""
-
-from flask import Flask, render_template, request
-from flask_babel import Babel, _
-
-
-class Config():
-    """Configuration class for babel"""
-
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
+""" Route module for the API """
+from flask import Flask, render_template
+from flask_babel import Babel
 
 app = Flask(__name__)
-app.config.from_object('1-app.Config')
 babel = Babel(app)
 
 
-@app.route("/")
-def home() -> str:
-    """Home page that displays a simple message"""
-    return render_template('2-index.html')
+class Config(object):
+    """ Available languages class """
+    LANGUAGES = ['en', 'fr']
+    # these are the inherent defaults just btw
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# set the above class object as the configuration for the app
+app.config.from_object('1-app.Config')
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+    Return:
+      - 1-index.html
+    """
+    return render_template('1-index.html')
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """Select Language for the current user"""
+    """ Determines best match for supported languages """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port="5000")
